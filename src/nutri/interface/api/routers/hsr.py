542c8aca3ac280561270994e1b4b3ce767306b77
Eaindra <eaindra.aye@atni.org>
2026-03-04 15:14:12 +0100

    Add test file under router for HSR

---
+++ src/nutri/interface/api/routers/hsr.py
@@ -0,0 +1,19 @@
+import csv
+import io
+
+from fastapi import APIRouter, HTTPException, UploadFile
+from pydantic import ValidationError
+
+from nutri.application.hsr import HsrCalculator
+from nutri.interface.schemas.nutriscore import (
+    ProductRequest,
+    HsrResponse,
+)
+
+router = APIRouter(prefix="", tags=[""])
+
+@router.post("")
+async def calculate_hsr(payload: ProductRequest) -> HsrResponse:
+    product = payload.to_product()
+    score, hsr_stars = HsrCalculator().get_results(product=product)
+    return HsrResponse(final_score=score, star_rating=hsr_stars)
