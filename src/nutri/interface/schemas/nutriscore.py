2c7488e2435d7ec29d210fe38e9d3abd204af38b
Valda Rahima <valda@gmail.com>
2026-03-19 09:39:37 +0100

    Add validation to restrict is_water and has_sweeteners to beverage category in the inerface/schemas layer

--- src/nutri/interface/schemas/nutriscore.py
+++ src/nutri/interface/schemas/nutriscore.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, model_validator
 
 from nutri.domain.nutriscore import NutriscoreGrade, Product, ProductCategory
 
@@ -14,6 +14,15 @@ class ProductRequest(BaseModel):
     has_sweeteners: bool = False
     is_water: bool = False
     category: ProductCategory
+
+    @model_validator(mode="after")
+    def validate_beverage_only_flags(self):
+        if self.category != ProductCategory.BEVERAGE:
+            if self.is_water:
+                raise ValueError("is_water can only be True for beverage products")
+            if self.has_sweeteners:
+                raise ValueError("has_sweeteners can only be True for beverage products")
+        return self
 
     def to_product(self) -> Product:
         return Product(
