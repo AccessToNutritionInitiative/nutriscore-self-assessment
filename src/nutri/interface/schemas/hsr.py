212372a55c1c5364510ec454f0284f458a6d19b1
jeremyarancio <jeremyarancio@gmail.com>
2026-03-25 12:09:08 +0100

    Format

--- src/nutri/interface/schemas/hsr.py
+++ src/nutri/interface/schemas/hsr.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, ConfigDict, Field, field_validator, model_validator
+from pydantic import BaseModel, Field, field_validator
 
 from nutri.domain.hsr import Product, ProductCategory
 
